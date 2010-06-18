1cab40439f9cc731d90ca00ec3670b9046feb425
chrisbull88 <chrisbull88@47fd52e2-b187-7f31-a4f7-a871415b517e>
2010-06-18 14:27:51 +0000

    - Re-jigged the UI (couldn't be doing with those absolute values :P) - Added an .ico (looks ok in 64x64, but any smaller is not great)

--- trunk/WiiTUIO/Properties/Resources.Designer.cs
+++ trunk/WiiTUIO/Properties/Resources.Designer.cs
@@ -59,5 +59,12 @@ namespace WiiTUIO.Properties {
                 resourceCulture = value;
             }
         }
+        
+        internal static System.Drawing.Icon icon {
+            get {
+                object obj = ResourceManager.GetObject("icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
